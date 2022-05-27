fde53c275d2d6e3d100d5cdb4fa9cbbb9f7f6137
wangning <wangning04@zhihu.com>
2022-05-27 09:25:39 +0800

    feat: 优化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <ul>
+  <div id="app">
     <h1>
-     测试 CI CD
+      测试 CI CD
     </h1>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
     <div>
       列表
     </div>
     <li v-for="(user) in users" :key="user.id">
-      {{user.id}}:{{user.name}}
+      {{ user.id }}:{{ user.name }}
     </li>
-  </ul>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
 export default {
   name: 'app',
-  data(){
+  data() {
     return {
-      users:[]
+      users: []
     }
   },
-  mounted(){
-    axios.get('http://47.95.114.11:3000/api/users').then(response=>{
+  mounted() {
+    axios.get('http://47.95.114.11:3000/api/users').then(response => {
       this.users = response.data;
     });
   }
@@ -30,4 +32,12 @@ export default {
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
