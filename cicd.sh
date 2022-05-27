592f5e35423b3ef744fcc998c59bda16a3a29a80
wangning <wangning04@zhihu.com>
2022-05-27 09:09:35 +0800

    feat: 优化

--- cicd.sh
+++ cicd.sh
@@ -7,11 +7,11 @@ git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "安装依赖"
-pnpm install
+npm install
 echo "删除旧 dist"
 rm -rf dist
 echo "打包最新代码"
-pnpm run build
+npm run build
 echo "开始构建镜像"
 docker build -t git-hub-vue .
 echo "删除旧容器"
