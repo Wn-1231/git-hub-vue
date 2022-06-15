92e5ec6f3d9f466ff7e33df86ea96c39a63c96d9
wangning <wangning04@zhihu.com>
2022-06-15 21:05:41 +0800

    feat: 测试兼容

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
 <template>
   <div id="app">
-    <hello msg="xxxxxx"/>
-    <h1>
-      测试 CI CD  xxx
-    </h1>
-    <img alt="Vue logo" src="./assets/logo.png">
+    <hello msg="xxxxxx" />
+    <h1>测试 CI CD xxx</h1>
+    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>
-      列表
-    </div>
-    <li v-for="(user) in users" :key="user.id">
-      {{ user.id }}:{{ user.name }}
-    </li>
+    <div>列表</div>
+    <li v-for="user in users" :key="user.id">{{ user.id }}:{{ user.name }}</li>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import hello from './components/HelloWorld.vue'
+import axios from "axios";
+import hello from "./components/HelloWorld.vue";
 export default {
-  name: 'app',
-  components:{
-    hello
+  name: "app",
+  components: {
+    hello,
   },
   data() {
     return {
-      users: []
-    }
+      users: [],
+    };
   },
   mounted() {
-    axios.get('http://47.95.114.11:3000/api/users').then(response => {
+    setTimeout(() => {
+      window.ysf("config", {
+        uid: 11111, // 用户Id
+        success() {
+          // 成功回调
+          window.ysf("open");
+        },
+        error() {},
+      });
+    }, 3000);
+    axios.get("http://47.95.114.11:3000/api/users").then(response => {
       this.users = response.data;
     });
-  }
-}
+  },
+};
 </script>
 
 <style>
