bd0dbdf37e61492d25a7709d9a8a6a2d26cf74d2
wangning <wangning04@zhihu.com>
2023-05-18 11:38:09 +0800

    feat: 添加 VConsole

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 import store from "./store";
+
+import VConsole from 'vconsole';
+
+new VConsole();
+
 Vue.config.productionTip = false
 
 new Vue({
